34a6477f3fa3e3f8c77396073374835499822fac
Aliaksei Pliutsinski <aliakseipliutsinski@MBP-Aliaksei.fritz.box>
2023-10-22 09:25:34 +0200

    feat: add solution for task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      this.currentNumber = Math.ceil( (this.min + this.max) / 2 );
+      return this.currentNumber;
     }
 
     lower() {
-
+      this.max = this.currentNumber;
+      this.currentNumber = Math.ceil( (this.min + this.max) / 2 );
     }
 
     greater() {
-
+      this.min = this.currentNumber;
+      this.currentNumber = Math.ceil( (this.min + this.max) / 2 );
     }
+
 }
 
 module.exports = GuessingGame;
